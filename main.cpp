677b1d1b91758584f92cfe662440d7e5fbdcd3dc
Trung <tpyhr2022@gmail.com>
2022-12-12 12:50:06 +0700

    Fixed Alg

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
-/*
- * Bot for Entropy (Codecup 2023)
- *
- * Author: D. Ngoc Anh Trung and N. Dang Dung
- *
- * Last updated: 02/12/2022
- */
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-#include <bits/stdc++.h>
-
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization("Ofast")
-#pragma GCC optimization("unroll-loops")
+#include <iostream>
+#include <vector>
+#include <map>
+#include <time.h>
+#include <string>
 
 using namespace std;
 
@@ -21,34 +16,69 @@ int maxdepth = 3;
 int unitbonus = 2;
 const double Infinity = 1e9;
 
-struct Piece
+typedef struct Piece
 {
-    int x, y, color, k;
+    int x, y;   // grid coordinates
+    int color;
+    int k;      // piece index in PlacedPieces
     Piece(int a, int b, int c, int _k) : x(a), y(b), color(c), k(_k) {}
-};
+} Piece;
+
+typedef struct Move
+{
+    string content;
+    int ChaosColor;
+    int OrderIndex;
+    // ! a bit arbitrary to have ChaosColor and OrderIndex in the same area.
+
+    Move() : content(string()), ChaosColor(-1), OrderIndex(-1) {}
+    Move(string m, int c, int o): content(m), ChaosColor(c), OrderIndex(o) {}
+} Move;
+
+typedef struct TTEntry
+{
+    double value;
+    int depth;
+    bool isValidEntry;
+    // add more flags here
+
+    TTEntry() : value(-1), depth(-1), isValidEntry(false) {}
+    TTEntry(double v, int d) : value(v), depth(d), isValidEntry(true) {}
+} TTEntry;
+
+typedef struct Flag
+{
+    // A generalised collection of flags for evaluation.
+    bool isSimulating;
+    Flag(): isSimulating(false) {}
+} Flag;
 
 vector<vector<int>> board;
 vector<Piece> PlacedPieces;
 
-unordered_map<string, pair<double, int>> TranspositionTable;
-unordered_map<string, double> TTMonteCarlo;
-vector<vector<vector<int>>> bonusEvaluations;
+map<vector<vector<int>>, TTEntry> TranspositionTable;
 
 int colourInput;
-string InputStr;
+string RawInput;
 
-string BestMoveForChaos();
-string BestMoveForOrder();
-double Search(int depth, int color, double alpha, double beta);
-double evaluate();
+void doMove(int color, Move move);
+void undoMove(int color, Move move);
 
-void doMove(int color, string move);
-void undoMove(int color, string move);
-string hashBoard();
+string BestMoveForSide(int color);
+double evaluate(bool GetHeuristic = true);
+double Search(int depth, int color, double alpha, double beta, Flag flag);
+
 
 void showBoard();
 
-pair<string, int> returnMove;
+vector<Move> returnMoves;
+double coeff[] = {0, 0, 1.21249, 1.8809, 2.45611, 2.64828, 3.47708, 3.37076};
+
+double fRand(double fMin, double fMax)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
 
 ///             === DRIVER CODE ===
 
@@ -58,47 +88,43 @@ int main()
     srand(time(NULL));
 
     board.assign(maxsize, vector<int>(maxsize, 0));
-    bonusEvaluations.assign(maxsize + 1, board);
 
-    while (getline(cin, InputStr))
+    while (getline(cin, RawInput))
     {
-        if (InputStr == "Start")
+        if (PlacedPieces.size() == 22)
+            maxdepth = 2;
+        returnMoves.clear();
+        if (RawInput == "Start")
         {
-            // chaos enhancements
+            //maxdepth = 2;
             continue;
         }
-        else if (InputStr == "Quit")
-        {
+        else if (RawInput == "Quit")
             break;
-        }
         else
         {
-            if (InputStr.length() == 1)
+            if (RawInput.length() == 1)
             {
-                colourInput = InputStr[0] - '0';
-                string move = BestMoveForChaos();
+                colourInput = RawInput[0] - '0';
+                string move = BestMoveForSide(-1);
                 cout << move << endl;
             }
-            else if (InputStr.length() == 4)
+            else if (RawInput.length() == 4)
             {
-                int foo = board[InputStr[0] - 'A'][InputStr[1] - 'a'];
+                int foo = board[RawInput[0] - 'A'][RawInput[1] - 'a'];
                 for (int i = 0; i < (int)PlacedPieces.size(); i++)
                 {
-                    if (PlacedPieces[i].x == InputStr[0] - 'A' && PlacedPieces[i].y == InputStr[1] - 'a')
+                    if (PlacedPieces[i].x == RawInput[0] - 'A' && PlacedPieces[i].y == RawInput[1] - 'a')
                     {
-                        PlacedPieces[i].x = InputStr[2] - 'A';
-                        PlacedPieces[i].y = InputStr[3] - 'a';
+                        doMove(1, Move(RawInput, -1, i));
                         break;
                     }
                 }
-                board[InputStr[0] - 'A'][InputStr[1] - 'a'] = 0;
-                board[InputStr[2] - 'A'][InputStr[3] - 'a'] = foo;
             }
-            else if (InputStr.length() == 3)
+            else if (RawInput.length() == 3)
             {
-                board[InputStr[1] - 'A'][InputStr[2] - 'a'] = InputStr[0] - '0';
-                PlacedPieces.push_back(Piece(InputStr[1] - 'A', InputStr[2] - 'a', InputStr[0] - '0', PlacedPieces.size()));
-                string move = BestMoveForOrder();
+                doMove(-1, Move(RawInput.substr(1, 2), RawInput[0] - '0', -1));
+                string move = BestMoveForSide(1);
                 cout << move << endl;
             }
         }
@@ -107,22 +133,11 @@ int main()
     return 0;
 }
 
-double checkWinner(int eval)
+double evaluate(bool GetHeuristic)
 {
-    if (120 + eval > 280 - eval)
-        return 1.0;
-    if (120 + eval < 280 - eval)
-        return -1.0;
-    return 0.5;
-}
+    double eval = 0;
 
-double evaluate()
-{
-    // Distribution of coefficients on the values of each length of palindromes
-
-    array<double, maxsize + 1> eval = {0, 0, 0, 0, 0, 0, 0, 0};
-    array<double, maxsize + 1> coeff = {0, 0, 1, 2, 3, 3, 4, 4};
-
+    vector<vector<int>> dp(maxsize, vector<int>(maxsize, 0));
     for (int len = 2; len <= maxsize; len++)
     {
         for (int i = 0; i < maxsize; i++)
@@ -130,22 +145,30 @@ double evaluate()
             for (int j = 0; j + len - 1 < maxsize; j++)
             {
                 bool flag = 1;
-                int bonusScore = 0;
                 int l = j;
                 int u = j + len - 1;
-                while (l <= u)
+                int inflatedAreas = 0;
+                while (l < u)
                 {
                     if (!board[i][l] || !board[i][u] || board[i][l] != board[i][u])
                     {
                         flag = 0;
                         break;
                     }
-                    bonusScore += (1 - (len & 1)) * bonusEvaluations[board[i][l]][i][l];
+
+                    if (l + 1 == board[i][l])
+                    {
+                        inflatedAreas++;
+                    }
+                    if (u + 1 == board[i][u])
+                    {
+                        inflatedAreas++;
+                    }
                     l++;
                     u--;
                 }
                 if (flag)
-                    eval[len] += len + bonusScore;
+                    eval += len * (GetHeuristic ? coeff[len] : 1) + (GetHeuristic && inflatedAreas >= len - 2 ? inflatedAreas * 0.25 : 0);
             }
         }
 
@@ -154,63 +177,68 @@ double evaluate()
             for (int i = 0; i + len - 1 < maxsize; i++)
             {
                 bool flag = 1;
-                int bonusScore = 0;
                 int l = i;
                 int u = i + len - 1;
-                while (l <= u)
+                int inflatedAreas = 0;
+                while (l < u)
                 {
                     if (!board[l][j] || !board[u][j] || board[l][j] != board[u][j])
                     {
                         flag = 0;
                         break;
                     }
-                    bonusScore += (1 - (len & 1)) * bonusEvaluations[board[l][j]][l][j];
+                    if (j + 1 == board[l][j])
+                    {
+                        inflatedAreas++;
+                    }
+                    if (j + 1 == board[u][j])
+                    {
+                        inflatedAreas++;
+                    }
                     l++;
                     u--;
                 }
                 if (flag)
-                    eval[len] += len + bonusScore;
+                    eval += len * (GetHeuristic ? coeff[len] : 1) + (GetHeuristic && inflatedAreas >= len - 2 ? inflatedAreas * 0.25 : 0);
             }
         }
     }
-    double value = 0;
-    for (int i = 2; i < eval.size(); i++)
-    {
-        value += eval[i] * coeff[i];
-    }
-
-    return value;
+    return eval;
 }
 
-void doMove(int side, pair<string, int> move)
+void doMove(int side, Move move)
 {
+
     if (side > 0)
     {
-        pair<int, int> oldPosition = {move.first[0] - 'A', move.first[1] - 'a'};
-        pair<int, int> newPosition = {move.first[2] - 'A', move.first[3] - 'a'};
-        board[oldPosition.first][oldPosition.second] = 0;
-        board[newPosition.first][newPosition.second] = PlacedPieces[move.second].color;
-        PlacedPieces[move.second].x = newPosition.first;
-        PlacedPieces[move.second].y = newPosition.second;
+        pair<int, int> oldPosition = {move.content[0] - 'A', move.content[1] - 'a'};
+        pair<int, int> newPosition = {move.content[2] - 'A', move.content[3] - 'a'};
+        swap(board[oldPosition.first][oldPosition.second], board[newPosition.first][newPosition.second]);
+        PlacedPieces[move.OrderIndex].x = newPosition.first;
+        PlacedPieces[move.OrderIndex].y = newPosition.second;
     }
     else
     {
-        board[move.first[0] - 'A'][move.first[1] - 'a'] = move.second;
-        PlacedPieces.push_back(Piece(move.first[0] - 'A', move.first[1] - 'a', move.second, PlacedPieces.size()));
+        int moveX = move.content[0] - 'A';
+        int moveY = move.content[1] - 'a';
+        board[moveX][moveY] = move.ChaosColor;
+        PlacedPieces.push_back(Piece(moveX, moveY, move.ChaosColor, PlacedPieces.size()));
     }
     return;
 }
 
-void undoMove(int side, pair<string, int> move)
+void undoMove(int side, Move move)
 {
     if (side > 0)
     {
-        pair<string, int> newMove = make_pair(move.first.substr(2, 2) + move.first.substr(0, 2), move.second);
+        Move newMove = Move(move.content.substr(2, 2) + move.content.substr(0, 2), -1, move.OrderIndex);
         doMove(side, newMove);
     }
     else
     {
-        board[move.first[0] - 'A'][move.first[1] - 'a'] = 0;
+        int moveX = move.content[0] - 'A';
+        int moveY = move.content[1] - 'a';
+        board[moveX][moveY] = 0;
         PlacedPieces.pop_back();
     }
 }
@@ -220,100 +248,41 @@ void showBoard()
     for (int i = 0; i < maxsize; i++)
     {
         for (int j = 0; j < maxsize; j++)
-        {
             cout << board[i][j];
-        }
         cout << endl;
     }
     cout << endl;
     return;
 }
 
-string hashBoard()
+string BestMoveForSide(int color)
 {
-    string s;
-    for (int i = 0; i < maxsize; i++)
-    {
-        for (int j = 0; j < maxsize; j++)
-        {
-            s += board[i][j] + '0';
-        }
-    }
-
-    return s;
-}
-
-string BestMoveForChaos()
-{
-    Search(0, -1, -Infinity, Infinity);
-    doMove(-1, returnMove);
-
-    int bestX = returnMove.first[0] - 'A';
-    int bestY = returnMove.first[1] - 'a';
-
-    for (int i = bestX % unitbonus; i < maxsize; i += unitbonus)
-    {
-        for (int j = bestY % unitbonus; j < maxsize; j += unitbonus)
-        {
-            bonusEvaluations[colourInput][i][j]++;
-        }
-    }
-
-    return string{char(bestX + 'A'), char(bestY + 'a')};
-}
-
-string BestMoveForOrder()
-{
-    Search(0, 1, -Infinity, Infinity);
-    doMove(1, returnMove);
-    int toMove = returnMove.second;
-    for (int i = PlacedPieces[toMove].x % unitbonus; i < maxsize; i += unitbonus)
-    {
-        for (int j = PlacedPieces[toMove].y % unitbonus; j < maxsize; j += unitbonus)
-        {
-            bonusEvaluations[PlacedPieces[toMove].color][i][j]--;
-        }
-    }
-    for (int i = PlacedPieces[toMove].x % unitbonus; i < maxsize; i += unitbonus)
-    {
-        for (int j = PlacedPieces[toMove].y % unitbonus; j < maxsize; j += unitbonus)
-        {
-            bonusEvaluations[PlacedPieces[toMove].color][i][j]++;
-        }
-    }
-
-    return returnMove.first;
+    Search(0, color, -Infinity, Infinity, Flag());
+    const auto returnMove = returnMoves[0];
+    doMove(color, returnMove);
+    return returnMove.content;
 }
 
 /// MAIN SEARCH ALGORITHM
 
-/*
-    implemented:
-    Minimax -> Negamax
-    alpha - beta pruning
-    transposition table
-    principal variation search
+// todo: iterative deepening and expected value algorithms
 
-    todo: iterative deepening and expected value algorithms
-*/
-
-void registerLegalMoves(vector<pair<string, int>> &LegalMoves, int color, int depth)
+void registerLegalMoves(vector<Move> &LegalMoves, int color, int depth)
 {
-    //cerr << "DEPTH = " << depth << " GENERATING MOVES... ";
     LegalMoves.clear();
     if (color > 0)
     {
-        for (int k = 0; k < PlacedPieces.size(); k++)
-        {
+        for (int k = 0; k < max((int)PlacedPieces.size(), 1); k++)
+        { 
             string oldPosition = string{char(PlacedPieces[k].x + 'A'), char(PlacedPieces[k].y + 'a')};
             for (int i = PlacedPieces[k].x; i < maxsize && (!board[i][PlacedPieces[k].y] || PlacedPieces[k].x == i); ++i)
-                LegalMoves.push_back(make_pair(oldPosition + string{char(i + 'A'), char(PlacedPieces[k].y + 'a')}, k));
+                LegalMoves.push_back(Move(oldPosition + string{char(i + 'A'), char(PlacedPieces[k].y + 'a')}, -1, k));
             for (int i = (PlacedPieces[k].x - 1); i >= 0 && !board[i][PlacedPieces[k].y]; --i)
-                LegalMoves.push_back(make_pair(oldPosition + string{char(i + 'A'), char(PlacedPieces[k].y + 'a')}, k));
+                LegalMoves.push_back(Move(oldPosition + string{char(i + 'A'), char(PlacedPieces[k].y + 'a')}, -1, k));
             for (int j = (PlacedPieces[k].y + 1); j < maxsize && !board[PlacedPieces[k].x][j]; ++j)
-                LegalMoves.push_back(make_pair(oldPosition + string{char(PlacedPieces[k].x + 'A'), char(j + 'a')}, k));
+                LegalMoves.push_back(Move(oldPosition + string{char(PlacedPieces[k].x + 'A'), char(j + 'a')}, -1, k));
             for (int j = (PlacedPieces[k].y - 1); j >= 0 && !board[PlacedPieces[k].x][j]; --j)
-                LegalMoves.push_back(make_pair(oldPosition + string{char(PlacedPieces[k].x + 'A'), char(j + 'a')}, k));
+                LegalMoves.push_back(Move(oldPosition + string{char(PlacedPieces[k].x + 'A'), char(j + 'a')}, -1, k));
         }
     }
     else
@@ -325,102 +294,58 @@ void registerLegalMoves(vector<pair<string, int>> &LegalMoves, int color, int de
                 if (board[i][j])
                     continue;
                 if (depth == 0)
-                {
-                    LegalMoves.push_back(make_pair(string{char(i + 'A'), char(j + 'a')}, colourInput));
-                }
+                    LegalMoves.push_back(Move(string{char(i + 'A'), char(j + 'a')}, colourInput, -1));
                 else
                 {
-                    for (int k = 1; k <= maxsize; k++)
-                    {
-                        LegalMoves.push_back(make_pair(string{char(i + 'A'), char(j + 'a')}, k));
-                    }
+                    for (int k = 1; k <= 7; k++)
+                        LegalMoves.push_back(Move(string{char(i + 'A'), char(j + 'a')}, k, -1));
                 }
             }
         }
     }
-    //cerr << "GENERATED MOVES! (size = " << LegalMoves.size() << ")" << endl;
     return;
 }
-double MonteCarlo()
+
+double Search(int depth, int color, double alpha, double beta, Flag flag)
 {
-    vector<vector<int>> cachedBoard = board;
-    for (int i = 0; i < maxsize; i++)
-        for (int j = 0; j < maxsize; j++)
-        {
-            if (!board[i][j])
-            {
-                board[i][j] = rand() % 7 + 1;
-            }
-        }
-    int score = evaluate();
-    board = cachedBoard;
-    return score;
-}
+    if (TranspositionTable[board].isValidEntry && depth >= TranspositionTable[board].depth)
+        return color * TranspositionTable[board].value;
 
-double Search(int depth, int color, double alpha, double beta)
-{
-    const auto s = hashBoard();
-    if (TranspositionTable[s].first && depth >= TranspositionTable[s].second)
+    if (depth >= maxdepth || PlacedPieces.size() >= 49)
     {
-        return color * (TranspositionTable[s].first);
+        TranspositionTable[board] = TTEntry(evaluate(), depth);
+        return color * TranspositionTable[board].value;
     }
-
-    if (depth >= maxdepth || PlacedPieces.size() == 49)
-    {
-        TranspositionTable[s] = {evaluate(), depth};
-        return color * (TranspositionTable[s].first);
-    }
-
-    bool isFirstNode = true;
     double score;
-    vector<pair<string, int>> LegalMoves;
+    vector<Move> LegalMoves;
+
     registerLegalMoves(LegalMoves, color, depth);
-    int bestVal = -Infinity;
+
+    if (color == 1)
+        colourInput = 0;
 
     for (const auto &move : LegalMoves)
     {
-        if (depth == 0)
-        {
-            doMove(color, move);
-            score = -Search(depth + 1, -color, -beta, -alpha);
-            undoMove(color, move);
+        doMove(color, move);
+        score = -Search(depth + 1, -color, -beta, -alpha, flag);
+        undoMove(color, move);
 
-            if (score >= alpha)
-            {
-                returnMove = move;
-                alpha = score;
-            }
+        // fail hard beta cutoff
+        if (score >= beta)
+            return beta;
+        
+        if (score == alpha && depth == 0)
+        {
+            returnMoves.push_back(move);
         }
-        else
+        
+        if (score > alpha)
         {
-            doMove(color, move);
-            if (isFirstNode)
+            if (depth == 0)
             {
-                score = -Search(depth + 1, -color, -beta, -alpha);
+                returnMoves = {move};
             }
-            else
-            {
-                score = -Search(depth + 1, -color, -alpha - 1, -alpha);
-
-                // fail soft and re-searching bestmove
-                if (alpha < score && score < beta)
-                {
-                    score = -Search(depth + 1, -color, -beta, -alpha);
-                }
-            }
-            undoMove(color, move);
-
-            // fail hard beta cutoff
-            if (score >= beta)
-            {
-                return beta;
-            }
-
-            if (score > alpha)
-            {
-                alpha = score;
-                isFirstNode = false;
-            }
+            alpha = score;
         }
     }
     return alpha;
